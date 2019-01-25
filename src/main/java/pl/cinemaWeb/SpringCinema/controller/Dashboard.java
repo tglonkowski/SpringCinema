378c1e14b40c6e8d84625667d26a98feba55e07f
hpiatkowski <hp@kropkikreski.com>
2019-01-26 00:02:48 +0100

    AddMoviePart

--- src/main/java/pl/cinemaWeb/SpringCinema/controller/Dashboard.java
+++ src/main/java/pl/cinemaWeb/SpringCinema/controller/Dashboard.java
@@ -1,7 +1,16 @@
 package pl.cinemaWeb.SpringCinema.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pl.cinemaWeb.SpringCinema.model.AgeCategoryEnum;
+import pl.cinemaWeb.SpringCinema.model.Movie;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class Dashboard {
@@ -9,5 +18,17 @@ public class Dashboard {
     @GetMapping("/dashboard")
     public String dashboard(){
         return "dashboard/index";
+    }
+
+    @GetMapping("/dashboard/addmovie")
+    public String addMovie(Model model){
+        Movie movie = new Movie();
+        model.addAttribute("movie", movie);
+        return "dashboard/addmovie";
+    }
+
+    @PostMapping("/dashboard/addmovie")
+    public String saveMovie(@ModelAttribute Movie movie, BindingResult bindingResult, Model model){
+        return "dashboard/addmovie";
     }
 }
