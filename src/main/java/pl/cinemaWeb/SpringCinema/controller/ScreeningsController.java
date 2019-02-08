fd10a25296bf1dd18711abbf1a17f6e8700c29a5
hpiatkowski <hp@kropkikreski.com>
2019-02-08 01:05:29 +0100

    repertuar_90

--- src/main/java/pl/cinemaWeb/SpringCinema/controller/ScreeningsController.java
+++ src/main/java/pl/cinemaWeb/SpringCinema/controller/ScreeningsController.java
@@ -3,26 +3,33 @@ package pl.cinemaWeb.SpringCinema.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import pl.cinemaWeb.SpringCinema.model.CinemaRoom;
+import pl.cinemaWeb.SpringCinema.model.Movie;
 import pl.cinemaWeb.SpringCinema.model.Screenings;
+import pl.cinemaWeb.SpringCinema.service.CinemaRoomService;
+import pl.cinemaWeb.SpringCinema.service.MovieService;
 import pl.cinemaWeb.SpringCinema.service.ScreeningService;
 
 import java.sql.Date;
 import java.util.List;
 
 @Controller
+@RequestMapping("/dashboard")
 public class ScreeningsController{
 
     ScreeningService screeningService;
+    MovieService movieService;
+    CinemaRoomService cinemaRoomService;
 
     @Autowired
-    public ScreeningsController(ScreeningService screeningService) {
+    public ScreeningsController(ScreeningService screeningService, MovieService movieService, CinemaRoomService cinemaRoomService) {
         this.screeningService = screeningService;
+        this.movieService = movieService;
+        this.cinemaRoomService = cinemaRoomService;
     }
 
-    @GetMapping("/dashboard/screenings")
+    @GetMapping("/screenings")
     public String screenings(Model model) {
 
         List<Screenings> screenings = screeningService.todayScreenings();
@@ -31,8 +38,46 @@ public class ScreeningsController{
         return "dashboard/screenings/timeline";
     }
 
-    @PostMapping("/dashborad/screenings")
-    public String showDay(@RequestParam("timelineDate") Date date, Model model){
+    @PostMapping("/screenings")
+    public String showByDay(@RequestParam("timelineDate") Date timelineDate, Model model){
+        List<Screenings> screenings = screeningService.screeningsByDate(timelineDate);
+        model.addAttribute("screenings", screenings);
+        model.addAttribute("timelineDate", timelineDate);
+        return "dashboard/screenings/timeline";
+    }
+
+    @PostMapping(value = "/screenings", params = "action=add")
+    public String addScreening(@RequestParam("timelineDate") Date timelineDate, Model model){
+        List<Screenings> screenings = screeningService.screeningsByDate(timelineDate);
+        List<String> movieTitles = movieService.getAllTitles();
+        List<CinemaRoom> cinemaRooms = cinemaRoomService.getAllRooms();
+        model.addAttribute("screenings", screenings);
+        model.addAttribute("movieTitles", movieTitles);
+        model.addAttribute("cinemaRooms", cinemaRooms);
+        model.addAttribute("timelineDate", timelineDate);
+        return "dashboard/screenings/timeline";
+    }
+
+    @PostMapping(value = "/screenings", params = "action=load")
+    public String loadMovie(@RequestParam("timelineDate") Date timelineDate, @RequestParam("titleLoad") String movieTitle, @RequestParam("roomLoad") long roomID, Model model){
+        Movie movieByTitle = movieService.movieByTitle(movieTitle);
+        CinemaRoom cinemaRoom = cinemaRoomService.getRoomById(roomID);
+        model.addAttribute("movieFromDB", movieByTitle);
+        model.addAttribute("selectedRoom", cinemaRoom);
+        model.addAttribute("newScreening", new Screenings());
+        model.addAttribute("timelineDate", timelineDate);
+
+        return "dashboard/screenings/timeline";
+    }
+
+    @PostMapping(value = "/screenings", params = "action=save")
+    public String saveScreening(@ModelAttribute Screenings newScreening, @RequestParam("timelineDate") Date timelineDate, Model model){
+        System.out.println("test1");
+        newScreening.setFreeSeats(newScreening.getCinemaRoom().getSeats());
+        System.out.println("save");
+        System.out.println(newScreening);
+        screeningService.save(newScreening);
+        model.addAttribute("added", "Seans dodany.");
 
         return "dashboard/screenings/timeline";
     }
